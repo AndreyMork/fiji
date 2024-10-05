05c6e4598ed2e3b788a4f63e2d15a54d15b1b1a0
AndreyMork <35698586+AndreyMork@users.noreply.github.com>
2024-10-05 11:36:52 +0000

    update

--- src/Errors.ts
+++ src/Errors.ts
@@ -1,12 +1,12 @@
 import * as Strukt from '@ayka/domistrukt';
 
 export class EnvNotLoadedError extends Strukt.error<{ name: string }, string>({
-	create: (name: string) => ({
-		name,
-	}),
-	message: ({ name }) => `ENV [${name}] is not loaded`,
+	create(name) {
+		return {
+			name,
+		};
+	},
+	message: (data) => `ENV [${data.name}] is not loaded`,
 }) {}
 
-export class InternalFijiError extends Strukt.error<string>({
-	message: (msg) => msg,
-}) {}
+export class InternalFijiError extends Strukt.staticError() {}
